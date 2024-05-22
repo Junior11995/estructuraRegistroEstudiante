18d036fdec480af1b7cb88c2212a4d5d33873540
Junior11995 <junior22ascinsero@gmail.com>
2024-05-22 19:46:00 -0400

    Agregar archivos de proyecto.

---
+++ estructuraRegistroEstudiante/estructuraRegistroEstudiante.cpp
@@ -0,0 +1,17 @@
+/*
+1. Registro de estudiantes:
+Crear una estructura llamada Estudiante que almacene la siguiente información sobre un
+estudiante:
+● Nombre (cadena de caracteres)
+● Apellido (cadena de caracteres)
+● Matrícula (número entero)
+● Promedio (número decimal)
+● Crear una función para leer los datos de un estudiante desde el teclado.
+● Crear una función para imprimir los datos de un estudiante en la consola.
+● Crear un menú que permita al usuario:
+○ Agregar un nuevo estudiante.
+○ Mostrar los datos de un estudiante existente (por matrícula).
+○ Mostrar la lista de todos los estudiantes.
+○ Salir del programa.
+*/
+
